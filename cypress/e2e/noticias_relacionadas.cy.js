6eadcfa1be92f2707bacaf9a24daccfbc1170f0b
Marília Liz <lizmarilia07@gmail.com>
2025-12-05 13:52:09 -0300

    teste e2e de noticias relacionadas

---
+++ cypress/e2e/noticias_relacionadas.cy.js
@@ -0,0 +1,28 @@
+describe('Teste E2E - Notícias Relacionadas', () => {
+  it('Abre uma notícia, rola até "relacionadas" e clica em uma relacionada', () => {
+    // Visita a home
+    cy.visit('/')
+
+    // Clica no primeiro título de notícia disponível
+    cy.get('.title').first().click({ force: true })
+
+    // Verifica que abriu uma página de notícia
+    cy.url().should('match', /\/[^\s]+\/$/)
+    cy.wait(800)
+
+    // Aguarda e rola até a lista de relacionadas
+    cy.get('.relacionadas-lista', { timeout: 10000 }).should('exist')
+    cy.get('.relacionadas-lista').scrollIntoView({ duration: 800 })
+    cy.wait(500)
+
+    // Verifica que existem itens relacionados
+    cy.get('.relacionadas-item').should('have.length.at.least', 1)
+
+    // Clica no primeiro item relacionado
+    cy.get('.relacionadas-item').first().click({ force: true })
+
+    // Verifica que navegou para a página da notícia clicada
+    cy.get('.noticia-titulo, .title', { timeout: 10000 }).should('be.visible')
+    cy.url().should('match', /\/[^\s]+\/$/)
+  })
+})
